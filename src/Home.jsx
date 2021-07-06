2b2e49a97086bdf4896818e3662b6901fdb8ef61
sonal.chaplot <sonal.chaplot@dieboldnixdorf.com>
2021-07-06 22:18:57 +0530

    Adding countdown code

---
+++ src/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import App from './App';
+import Countdown from './Countdown';
+export default function Home() {
+  return (
+    <div>
+      <Countdown />
+    </div>
+  );
+}
